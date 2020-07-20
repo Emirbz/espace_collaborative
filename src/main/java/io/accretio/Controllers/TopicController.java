5cd7846a4c1adaa9ef8501fb311e39c757c1103f
Skander Chamakhi <skander.chamakhi@esprit.tn>
2020-07-20 11:47:03 +0100

    Add Title and description to Topic + Cleanup

--- src/main/java/io/accretio/Controllers/TopicController.java
+++ src/main/java/io/accretio/Controllers/TopicController.java
@@ -1,25 +1,29 @@
 package io.accretio.Controllers;
 
-import io.accretio.Models.Topic;
-import io.accretio.Models.TopicCategory;
-import io.accretio.Services.TopicCategoryService;
-import io.accretio.Services.TopicService;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
-import java.util.List;
+import io.accretio.Models.Topic;
+import io.accretio.Models.TopicCategory;
+import io.accretio.Services.TopicCategoryService;
+import io.accretio.Services.TopicService;
 
 @ApplicationScoped
 @Path("topic")
--- src/main/java/io/accretio/Models/Room.java
+++ src/main/java/io/accretio/Models/Room.java
@@ -1,15 +1,25 @@
 package io.accretio.Models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 
 @Table
